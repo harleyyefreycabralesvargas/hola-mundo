b0b411d5630f42d8a2ef779174f5ba99167878d9
Harley Yefrey Cabrales Vargas <arleycabralesvargas@gmail.com>
2026-06-16 22:55:37 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hello world")
+from colorama import Fore, Style, init
+
+# Inicializa colorama
+init(autoreset=True)
+
+print(Fore.GREEN + "[OK] El entorno virtual del Worker funciona correctamente.")
+print(Fore.CYAN + "[INFO] Procesando datos de prueba del orquestador...")
+print(Fore.YELLOW + "[WARN] Esto es una alerta de prueba colorida.")
+print(Fore.MAGENTA + "=== FIN DEL PROCESO ===")
